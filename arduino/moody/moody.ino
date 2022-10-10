3023549403872a50b84f59d08c0920f7d6485ed5
tsomic <keller.nils99@gmail.com>
2022-10-10 23:04:27 +0200

    add temperature and shock

--- arduino/moody/moody.ino
+++ arduino/moody/moody.ino
@@ -5,17 +5,26 @@
 #include "faces.h"
 #include "facesConfig.h"
 
-#define DEFAULT_FACE_DELAY 1000
+#define DEFAULT_FACE_DELAY 10
+#define TEMP_HOT_THRESHOLD 27
+#define TEMP_COLD_THRESHOLD 8
+
+#define SHOCK_PIN 13
 
 GY21 sht;
 Adafruit_SSD1306 display(128, 64, &Wire, -1); 
 
 uint8_t currentFace;
-uint8_t numberRepeats;
+int8_t numberRepeats;
+uint16_t faceTimer = DEFAULT_FACE_DELAY;
+
+bool shockSensorSate = 0;
+float temp;
 
 void loopFace(uint8_t face, uint16_t faceDelay = DEFAULT_FACE_DELAY);
 
 void setup() {
+  pinMode(SHOCK_PIN, INPUT);
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
@@ -23,16 +32,38 @@ void setup() {
   randomSeed(analogRead(0));
   resetNumberRepeats();
   selectStartingFace();
-  //Serial.begin(9600);
+  Serial.begin(9600);
   //while (!Serial);
 }
 
 void loop() {
+  shockSensorSate = digitalRead(SHOCK_PIN);
+  Serial.println(shockSensorSate);
+  
   loopFace(currentFace);
-  numberRepeats--;
-  if (numberRepeats == 0) {
+
+  if (faceTimer <= 0) {
+    numberRepeats--;
+    faceTimer = DEFAULT_FACE_DELAY;
+  }
+  
+  if (numberRepeats <= 0) {
     resetNumberRepeats();
     selectNextFace();
+  }
+
+  temp = sht.GY21_Temperature();
+  //Serial.println(temp);
+  if (temp > TEMP_HOT_THRESHOLD) {
+    currentFace = HOT;
+  } else if (currentFace == HOT) {
+    numberRepeats = 0;
+  }
+
+  if (temp < TEMP_COLD_THRESHOLD) {
+    currentFace = COLD;
+  } else if (currentFace == COLD) {
+    numberRepeats = 0;
   }
 }
 
@@ -49,17 +80,14 @@ void selectNextFace() {
   int8_t nextFace;
   do {
     nextFace = nextFaces[currentFace][random(3)];
-    Serial.println(nextFace);
   } while (nextFace == -1);
   
   currentFace = nextFace;
 }
 
 void loopFace(uint8_t face, uint16_t faceDelay = DEFAULT_FACE_DELAY) {
-  for (uint8_t i = 0; i < 2; i++) {
-    drawFace(allFaces[face][i]);
-    delay(faceDelay);
-  }
+  faceTimer--;
+  drawFace(allFaces[face][numberRepeats % 2]);
 }
 
 void drawFace(const uint8_t * bmp) {
